26bbb1379a8dea752d878708082991ea834b1c30
Sivan Abas <sivsiva3@gmail.com>
2017-05-16 17:35:38 +0300

    Supernatural Starter Kit

---
+++ SPN/SignUp.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace SPN
+{
+    public partial class SignUp : System.Web.UI.Page
+    {
+        private string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Database.mdf;Integrated Security=True";
+
+        public string message = "";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.HttpMethod == "POST")
+            {
+                string fname = Request.Form["fname"];
+                string lname = Request.Form["lname"];
+                string nname = Request.Form["nname"];
+                string email = Request.Form["email"];
+                string pass = Request.Form["pass"];
+                string active = Request.Form["active"];
+                string phone = Request.Form["phone"];
+
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+
+                SqlCommand command = connection.CreateCommand();
+                command.CommandText = String.Format("SELECT * FROM Users WHERE [Username] = '" + nname + "'");
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    message = "Username is already taken";
+                    reader.Close();
+                }
+                else
+                {
+                    reader.Close();
+                    command.CommandText = String.Format("INSERT INTO Users VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}');", nname, pass, fname, lname, email, phone, active);
+                    command.ExecuteNonQuery();
+
+                    Response.Redirect("SignIn.aspx");
+                }
+                reader.Close();
+                connection.Close();
+            }
+        }
+    }
+}
